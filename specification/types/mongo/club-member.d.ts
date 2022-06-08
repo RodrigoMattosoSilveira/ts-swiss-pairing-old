b0de60b32bf546ce788222cac5ca562cb92fb6b3
Rodrigo Silveira <r3850365@gmail.com>
2022-06-08 01:37:24 -0700

    Replaced the Club Member cell attribute with email attribute

--- specification/types/mongo/club-member.d.ts
+++ specification/types/mongo/club-member.d.ts
@@ -4,11 +4,10 @@
 interface IClubMember {
     id: string,         // 8 chars, random string
     first: string,
-    middle: string,
     last: string,
-    cell: string,
+    email: string,
     rating: number,     // 1200 default
-    active: boolean     // TRUE default
+    status: boolean     // active, inactive
 }
 
 export { IClubMember }
